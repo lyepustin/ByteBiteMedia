32eaeb4ff0ed50209ef192ed1d9de545ffbe7c97
Denys L <lyepustin@tutanota.com>
2024-02-18 22:20:56 +0100

    added some extra functionality

--- tools/pipeline.py
+++ tools/pipeline.py
@@ -5,15 +5,12 @@ from convert_utilities import convert_mp4_to_wav
 import os
 from dotenv import load_dotenv, find_dotenv
 
-
-video_url = "https://youtu.be/6At10g4UlmE"
-download_audio(video_url, os.getenv("MEDIA_MP4_FOLDER"))
 load_dotenv(find_dotenv())
 
-song_title = os.getenv("YOUTUBE_VIDEO_TITLE")
-print(song_title)
+video_url = os.getenv("YOUTUBE_URL_VIDEO")
+song_title = download_audio(video_url, os.getenv("MEDIA_MP4_FOLDER"))
 convert_mp4_to_wav(song_title)
 
-win_lengths = [1, 5, 10, 15, 20, 30, 40, 60]
+win_lengths = [30, 60, 90]
 min_lengths = [1]
 process_audio_data(song_title, win_lengths, min_lengths)
--- tools/ytb_download.py
+++ tools/ytb_download.py
@@ -1,44 +1,25 @@
-import os
 from pytube import YouTube
-from dotenv import load_dotenv, find_dotenv
-load_dotenv(find_dotenv())
+from pytube.helpers import safe_filename
 
-def update_env_variable(variable_name, new_value):
+
+def get_title_video(url):
     try:
-        # Open the .env file in read mode
-        with open(find_dotenv(), 'r') as file:
-            lines = file.readlines()
-
-        print(lines)
-
-        # Find and update the specified variable
-        for i, line in enumerate(lines):
-            if line.startswith(f"{variable_name}="):
-                lines[i] = f'{variable_name}="{new_value}"\n'
-                break
-
-        # Open the .env file in write mode and write the updated lines
-        with open(find_dotenv(), 'w') as file:
-            file.writelines(lines)
-
-        print(f"{variable_name} updated successfully: {new_value}")
+        yt = YouTube(url)
+        filename = safe_filename(yt.title)
+        return filename
     except Exception as e:
-        print(f"Error updating {variable_name}: {e}")
+        print(f"Error: {e}")
 
 
 def download_audio(url, output_path='media'):
     try:
-        # Create a YouTube object
         yt = YouTube(url)
-
-        # Get the highest resolution stream
+        title = get_title_video(url)
         audio_stream = yt.streams.get_audio_only()
-
-        # Download the video to the specified output path
-        audio_stream.download(output_path)
-
-        print(f"{yt.title} downloaded successfully to {output_path}")
-        
-        update_env_variable("YOUTUBE_VIDEO_TITLE", yt.title)
+        audio_stream.download(
+            output_path=output_path,
+            filename=f"{title}.{audio_stream.subtype}")
+        print(f"{title} downloaded successfully to {output_path}")
+        return title
     except Exception as e:
         print(f"Error: {e}")
